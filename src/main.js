12c0782856d1252c0c59292a41796b91f4beb0be
dffxd-suntra <qawseswa1931@gmail.com>
2023-08-28 11:59:00 +0000

    new file:   frontend/.gitignore 	new file:   frontend/.vscode/extensions.json 	new file:   frontend/README.md 	new file:   frontend/index.html 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/vite.svg 	new file:   frontend/src/App.vue 	new file:   frontend/src/assets/vue.svg 	new file:   frontend/src/components/HelloWorld.vue 	new file:   frontend/src/main.js 	new file:   frontend/src/style.css 	new file:   frontend/vite.config.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+const Http = require("http");
 const Koa = require('koa');
 const Router = require('@koa/router');
+const SocketIO = require("socket.io");
 
 const app = new Koa();
 const router = new Router();
+const httpServer = Http.createServer(app.callback());
+const io = new SocketIO.Server(httpServer, {});
 
 router.get('/', (ctx, next) => {
     ctx.body = "<h1>Hello World!</h1>";
@@ -11,5 +15,6 @@ router.get('/', (ctx, next) => {
 
 app
     .use(router.routes())
-    .use(router.allowedMethods())
-    .listen(3000);
+    .use(router.allowedMethods());
+
+httpServer.listen(3000);

f8f7677b2f670e88cb4e914255e22b94b151418b
dffxd-suntra <qawseswa1931@gmail.com>
2023-09-02 13:13:36 +0000

    modified:   src/method/AddUser.js

--- src/method/AddUser.js
+++ src/method/AddUser.js
@@ -2,7 +2,7 @@ const db = require("../library/sqlite3");
 const addUserAuth = require("./AddUserAuth");
 const moment = require("moment");
 
-module.exports = function addUser({ nickname = "机器人-" + moment().format("YYYYMMDD"), sex = "boy", avatar = null, role = "user", status = "active" } = {}, auth = []) {
+module.exports = function addUser({ nickname = "机器人-" + moment().format("YYYYMMDD"), sex = "boy", avatar = null, role = "user", status = 1 } = {}, auth = []) {
     db.prepare("BEGIN TRANSACTION").run();
     const stmt = db.prepare("INSERT INTO users(nickname, sex, avatar, role, status) VALUES($nickname, $sex, $avatar, $role, $status);");
     stmt.run({ nickname, sex, avatar, role, status });
